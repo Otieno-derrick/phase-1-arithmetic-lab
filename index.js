c54b7d40797461ec54c3e18cd95d066f1e878abf
derrique <otieno.derrick@student.moringaschool.com>
2022-06-12 17:48:28 +0300

    done

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 
 // Write your code here
-let multiply = num1 * num2;
+
 let num1 = 31;
 let num2 = 2;
+let multiply = num1 * num2;
 print (multiply);
 
 let random = Math.floor(Math.random() * 10);
