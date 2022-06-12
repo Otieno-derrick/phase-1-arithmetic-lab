878afd4ac04f51f7eff7dbf922b6558b0e489c7a
derrique <otieno.derrick@student.moringaschool.com>
2022-06-12 17:47:03 +0300

    done

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+let multiply = num1 * num2;
+let num1 = 31;
+let num2 = 2;
+print (multiply);
+
+let random = Math.floor(Math.random() * 10);
+
+
+let num3 = 14;
+let num4 = 5;
+let mod = num3 % num4;
+ print (mod);
+
+
+let max = Math.max(5,10,15,20,4,16);
